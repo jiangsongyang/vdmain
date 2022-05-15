9fbf27e1623c650c249c507d82bafb53453de810
jiangsongyang <2744856707@qq.com>
2022-05-15 21:56:04 +0800

    feat: set commit-msg hook

--- scripts/commit-msg.js
+++ scripts/commit-msg.js
@@ -1,1 +1,25 @@
-process.exit(1)
+// Invoked on the commit-msg git hook by yorkie.
+
+import chalk from 'chalk'
+import { readFileSync } from 'fs'
+
+const msgPath = process.env.GIT_PARAMS
+const msg = readFileSync(msgPath, 'utf-8').trim()
+
+console.log(msgPath, 'msgPath')
+
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
+      chalk.red(`  See .github/commit-convention.md for more details.\n`)
+  )
+  process.exit(1)
+}
